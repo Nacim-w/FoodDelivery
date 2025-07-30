984329d7d772f501a1dad0e30055de744c9067c7
Nacim-w <msallem.nacim@gmail.com>
2025-07-30 12:18:31 +0100

    added search to all restaurants view

--- lib/features/restaurant/presentation/views/all_restaurants_view.dart
+++ lib/features/restaurant/presentation/views/all_restaurants_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:legy/core/extension/text_style_extension.dart';
-import 'package:legy/core/res/styles/text.dart';
 import 'package:legy/features/restaurant/presentation/widgets/all_restaurants_widget.dart';
 
 class AllRestaurantsView extends StatelessWidget {
@@ -10,12 +8,6 @@ class AllRestaurantsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Restaurants", style: TextStyles.textSemiBoldLarge.black1),
-        centerTitle: true,
-      ),
-      body: AllRestaurantsWidget(),
-    );
+    return AllRestaurantsWidget();
   }
 }
